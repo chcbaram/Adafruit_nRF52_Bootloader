7bec23d5b99c846b7dba8d76c4920b3adacb68cf
Hancheol Cho <chcbaram@gmail.com>
2026-07-08 23:36:46 +0900

    wish65 추가

--- src/boards/baram_nrf52_wish65/board.h
+++ src/boards/baram_nrf52_wish65/board.h
@@ -22,53 +22,43 @@
  * THE SOFTWARE.
  */
 
-#ifndef _BARAM_NU40_DEVKIT_H
-#define _BARAM_NU40_DEVKIT_H
+#ifndef _BARAM_NRF52_WISH60_H
+#define _BARAM_NRF52_WISH60_H
 
 #define _PINNUM(port, pin)    ((port)*32 + (pin))
 
 #define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_3V3
 
-
-#define DEVICE_NAME           "BaramDFU"
-
-
 /*------------------------------------------------------------------*/
 /* LED
  *------------------------------------------------------------------*/
-#define LEDS_NUMBER       4
-#define LED_PRIMARY_PIN   _PINNUM(0, 13) 
-#define LED_SECONDARY_PIN _PINNUM(0, 14) 
-#define LED_3_PIN         _PINNUM(0, 15) 
-#define LED_4_PIN         _PINNUM(0, 16) 
-#define LED_STATE_ON      0
+#define LEDS_NUMBER       1
+#define LED_PRIMARY_PIN   _PINNUM(1, 9) 
+#define LED_STATE_ON      1
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER    4  
-#define BUTTON_1          _PINNUM(0, 11) 
-#define BUTTON_2          _PINNUM(0, 12) 
-#define BUTTON_3          _PINNUM(0, 13) 
-#define BUTTON_4          _PINNUM(0, 14) 
+#define BUTTONS_NUMBER    1  // none connected at all
+#define BUTTON_1          _PINNUM(0, 18)  // unusable: RESET
 #define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
 #define BLEDIS_MANUFACTURER  "BARAM"
-#define BLEDIS_MODEL         "baram-nu40-devkit"
+#define BLEDIS_MODEL         "baram-wish65"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
 #define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x00B4
-#define USB_DESC_CDC_ONLY_PID  0x00B4
+#define USB_DESC_UF2_PID       0x00B5
+#define USB_DESC_CDC_ONLY_PID  0x00B5
 
-#define UF2_PRODUCT_NAME  "BARAM-NU40-DEVKIT"
-#define UF2_VOLUME_LABEL  "BARAM-NU40"
-#define UF2_BOARD_ID      "BARAM-NU40"
-#define UF2_INDEX_URL     "https://github.com/chcbaram/Adafruit_nRF52_Bootloader"
+#define UF2_PRODUCT_NAME  "BARAM-WISH65"
+#define UF2_VOLUME_LABEL  "WISH65-BLE"
+#define UF2_BOARD_ID      "WISH65-BLE"
+#define UF2_INDEX_URL     "https://github.com/chcbaram"
 
 #endif 
